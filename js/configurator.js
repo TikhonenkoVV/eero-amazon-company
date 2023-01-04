f4ea43f09bcd4a30199db463ea5d91dc44ae47f5
TikhonenkoVV <113926273+TikhonenkoVV@users.noreply.github.com>
2023-01-04 02:05:57 +0200

    configurator btns

--- js/configurator.js
+++ js/configurator.js
@@ -1,59 +1,84 @@
 (() => {
-    const group = document.querySelector('.configurator__group');
-    const chkboxS = document.querySelector('.configurator__checkbox--smaller');
-    const chkboxM = document.querySelector('.configurator__checkbox--medium');
-    const chkboxL = document.querySelector('.configurator__checkbox--larger');
+    const labelMath = document.querySelectorAll('.configurator__label');
+    const curConfigurator = document.querySelector('.configurator__controls--home');
+    const labelArr = curConfigurator.getElementsByClassName('configurator__label');
+    const nextBtn = document.querySelector('.configurator__btn--next');
+    const prevBtn = document.querySelector('.configurator__btn--preview');
     const imgBgr = document.querySelector('.configurator__img');
-    let pathA = './img/';
-    var srcA = imgBgr.src;
-    var srcB = '';
-    var srcC = '';
-    var srcD = '';
+    let pathA = './img/selection/';
+    let defaultSrc = imgBgr.src;
+    let pageCounter = 0;
+    let isDisabled = true;
+    let pageClass;
 
-    chkboxS.addEventListener('change', event => {
-        imgBgr.src = srcB;
-        srcA = srcB;
-        console.log(srcA);
+    function strFoo(num) {
+        switch (pageCounter) {
+            case 0:
+                pageClass = 'home';
+                break;
+            case 1:
+                pageClass = 'gadjets';
+                break;
+            case 2:
+                pageClass = 'speed';
+                break;
+            case 3:
+                pageClass = 'buy';
+                break;
+        }
+        return pageClass;
+    }
+
+    Array.from(labelArr, lbl => {
+        let str;
+        lbl.addEventListener('click', element => {
+            labelMath.forEach(el => {
+                el.classList.remove('configurator__label--current');
+            });
+            element.target.classList.add('configurator__label--current');
+            console.log(str);
+            imgBgr.src = str;
+            defaultSrc = str;
+            nextBtn.classList.remove('disabled');
+            isDisabled = false;
+        });
+        lbl.addEventListener('mouseover', event => {
+            str = pathA;
+            let lblId = lbl.getAttribute('id');
+            str += lblId + '@1x.png';
+            imgBgr.src = str;
+        });
+        lbl.addEventListener('mouseout', event => {
+            imgBgr.src = defaultSrc;
+            str = '';
+        });
     });
 
-    chkboxM.addEventListener('change', event => {
-        imgBgr.src = srcC;
-        srcA = srcC;
-        console.log(srcA);
-    });
-
-    chkboxL.addEventListener('change', event => {
-        imgBgr.src = srcD;
-        srcA = srcD;
-        console.log(srcA);
-    });
-
-    group.addEventListener('mouseover', event => {
-        var strA = '';
-        var strB = '';
-        if (event.target.classList.contains('configurator__label--smaller')) {
-            strA = 'home-1.png';
-            strB = pathA + strA;
-            imgBgr.src = strB;
-            srcB = strB;
-        } else {
-            if (event.target.classList.contains('configurator__label--medium')) {
-                strA = 'home-2.png';
-                strB = pathA + strA;
-                imgBgr.src = strB;
-                srcC = strB;
-            } else {
-                if (event.target.classList.contains('configurator__label--larger')) {
-                    strA = 'home-3.png';
-                    strB = pathA + strA;
-                    imgBgr.src = strB;
-                    srcD = strB;
+    nextBtn.addEventListener('click', next => {
+        if (!isDisabled && pageCounter < 3) {
+            pageCounter += 1;
+            if (pageCounter >= 1) {
+                prevBtn.classList.remove('visually-hidden');
+                if (pageCounter === 3) {
+                    next.target.innerHTML = 'show result';
+                } else {
+                    next.target.innerHTML = 'next';
                 }
             }
+            console.log(pageCounter);
+            console.log(strFoo(pageCounter));
         }
     });
 
-    group.addEventListener('mouseout', event => {
-        imgBgr.src = srcA;
+    prevBtn.addEventListener('click', preview => {
+        if (pageCounter > 0) {
+            pageCounter -= 1;
+            nextBtn.innerHTML = 'next';
+            console.log(pageCounter);
+        }
+        if (pageCounter === 0) {
+            preview.target.classList.add('visually-hidden');
+            console.log(pageCounter);
+        }
     });
 })();
